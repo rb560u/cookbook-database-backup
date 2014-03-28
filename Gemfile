9030e1e9e3df5930271c639983f67cb1e7856ba4
torandu <sean@torandu.ch>
2014-03-28 09:29:28 -0700

    Update dependencies

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 # -*- mode: ruby -*-
 # vim: set ft=ruby :
 
-# Gemfile-common
-
 source 'https://rubygems.org'
 
 gem 'berkshelf',  '~> 2.0.14'
@@ -10,7 +8,7 @@ gem 'chef',       '~> 11.4.4'
 gem 'chefspec',   '~> 1.3.1'
 gem 'foodcritic', '~> 3.0.3'
 gem 'json',       '<= 1.7.7'  # Chef 11 dependency
-gem 'rake',       '~> 10.1.1'
-gem 'rubocop',    '~> 0.19.0'
+gem 'rake',       '~> 10.2.2'
+gem 'rubocop',    '~> 0.19.1'
 gem 'pry'
 gem 'awesome_print'
--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,14 @@
-name             "database-backup"
-maintainer       "Jay Pipes"
-maintainer_email "jaypipes@att.com"
-license          "Apache 2.0"
-description      "Installs/Configures Percona XtraBackup and sets up automated backup of MySQL databases"
+name             'database-backup'
+maintainer       'Jay Pipes'
+maintainer_email 'jaypipes@att.com'
+license          'Apache 2.0'
+description      'Installs/Configures Percona XtraBackup and sets up automated backup of MySQL databases'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          '0.2.3'
 
-recipe           "database-backup", "Installs Percona XtraBackup and sets up backups"
+recipe           'database-backup', 'Installs Percona XtraBackup and sets up backups'
 
-supports         "ubuntu", ">= 12.04"
+supports         'ubuntu', '>= 12.04'
 
-depends "apt"
-depends "yum"
+depends 'apt'
+depends 'yum'
